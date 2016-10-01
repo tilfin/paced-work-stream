ba8d8179dd34dc50179b7adc44595a076aa7a04e
Toshimitsu Takahashi <toshi@tilfin.com>
2016-10-01 19:43:06 +0900

    Fix when workPromise do not return promises

--- test/work_promise.test.js
+++ test/work_promise.test.js
@@ -3,6 +3,8 @@
 const chai = require('chai');
 const assert = chai.assert;
 const es = require('event-stream');
+const devnull = require('dev-null');
+
 const PacedWorkStream = require('../lib/main');
 
 describe('PacedWorkStream', () => {
@@ -30,7 +32,7 @@ describe('PacedWorkStream', () => {
         });
 
       const reader = es.readArray([11, 12, 21, 22, 31])
-      reader.pipe(pwStream);
+      reader.pipe(pwStream).pipe(devnull({ objectMode: true }));
     });
 
     it('raises done event', (done) => {
@@ -62,6 +64,59 @@ describe('PacedWorkStream', () => {
       const writer = es.writeArray(function(err, array) {
         assert.deepEqual(array, ['11', '12-1', '12-2', '12-3', '12-4', '12-5',
             '21', '22-1', '22-2', '22-3', '22-4', '22-5', '31']);
+      });
+
+      reader.pipe(pwStream).pipe(writer);
+    });
+  });
+
+  context('workPromise returns no promises sometimes', () => {
+    it('raises done event', (done) => {
+      const pwStream = new PacedWorkStream({
+          concurrency: 3,
+          workMS: 0
+        }, function(item) {
+          if (item % 10 === 2) {
+            return Promise.resolve(item.toString())
+                    .then((r) => { this.countTag('workDone'); return r; });
+          }
+        })
+        .on('done', function() {
+          assert.deepEqual(this.tagCounts, { workDone: 2 });
+          done();
+        }).on('error', (err) => {
+          assert.ifError(err);
+          done();
+        });
+
+      const reader = es.readArray([11, 12, 21, 22, 31])
+      const writer = es.writeArray(function(err, array) {
+        assert.deepEqual(array, ['12', '22']);
+      });
+
+      reader.pipe(pwStream).pipe(writer);
+    });
+  });
+
+  context('workPromise does not return promises at all', () => {
+    it('raises done event', (done) => {
+      const pwStream = new PacedWorkStream({
+          concurrency: 3,
+          workMS: 0
+        }, function(item) {
+          return;
+        })
+        .on('done', function() {
+          assert.deepEqual(this.tagCounts, {});
+          done();
+        }).on('error', (err) => {
+          assert.ifError(err);
+          done();
+        });
+
+      const reader = es.readArray([11, 12, 21, 22, 31])
+      const writer = es.writeArray(function(err, array) {
+        assert.deepEqual(array, []);
       });
 
       reader.pipe(pwStream).pipe(writer);
